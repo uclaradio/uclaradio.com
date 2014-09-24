1f747553099e014afafc208630307f3a1b64e072
Zach North <zrnorth@gmail.com>
2014-09-24 16:43:59 -0700

    added date checking for colors

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,63 @@ function squarify(element) {
     $(element).height($(element).width());
   }
 }
+// given the time of day, returns a random color scheme 
+// matching it.
+function getRandColorScheme(timeOfDay) {
+  var possColors, choice;
+  switch(timeOfDay) {
+    case 'night': // blacks, greys
+      {
+        possColors = ['Greys'];
+        break;
+      }
+    case 'morning': // oranges, yellows, reds, greens
+      {
+        possColors = ['Reds', 'Oranges', 'YlOrBr', 'YlOrRd', 'OrRd', 
+            'PuRd'];
+        break;
+      }
+    case 'noon': // greens, purples, blues, yellows
+      {
+        possColors = ['Greens', 'RdPu', 'PuRd', 'BrBG', 'PuOr', 'PiYG', 
+            'Spectral', 'YlGn'];
+        break;
+      }
+    case 'evening': // blues, cool colors
+      {
+        possColors = ['YlGnBu', 'GnBu', 'PuBuGn', 'PuBu', 'Purples', 'Blues',
+                   'RdPu', 'Greens'];
+        break;
+      }
+  }
+  choice = possColors[Math.floor(Math.random()*possColors.length)];
+  return choice;
+}
+
+function getColorSchemeFromTime() {
+  var hour = new Date().getUTCHours();
+
+  // UCLA is located 7 hours behind UTC. Use UTC time so our 
+  // background colors will be synchronized wherever you are in the world.
+  var timeOfDay = null;
+
+  // Nighttime: 9pm - 6am == 21 - 6 PDT == 4 - 13 UTC
+  if (hour >= 4 && hour < 13) {
+    return getRandColorScheme('night');
+  } 
+  // Morning: 6am - 9am == 6-9 PDT == 13-16 UTC
+  else if (hour >= 13 && hour < 16) {
+    return getRandColorScheme('morning');
+  }
+  // Noon: 9am - 5pm == 9-17 PDT == 16-00 UTC
+  else if (hour >= 16) {
+    return getRandColorScheme('noon');
+  }
+  // Evening: 5pm-9pm == 17-21 PDT == 00-04 UTC
+  else {
+    return getRandColorScheme('evening');
+  }
+}
 
 $(function() {
 
@@ -24,8 +81,10 @@ $(function() {
   squarify($('#play-button')); 
 
   // Generate that background image!
-  var t = new Trianglify({noiseIntensity: 0.0, cellsize: 175});
+  var colorScheme = getColorSchemeFromTime();
+  var colors = Trianglify.colorbrewer[colorScheme][9];
+  var t = new Trianglify({noiseIntensity: 0.0, cellsize: 175, x_gradient: colors});
   var pattern = t.generate(4000, 3000);
   document.body.setAttribute('style', 'background: ' + 
-     pattern.dataUrl + ' no-repeat center center fixed');
+    pattern.dataUrl + ' no-repeat center center fixed');
 });
