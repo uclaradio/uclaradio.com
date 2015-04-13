e4424e0d7b8281d10bbccca821ef29f49d3145d2
MatteoVH <matteovh@ucla.edu>
2015-04-13 13:33:39 -0700

    Fixed database access in db module. You can now get a dj blurb by timeslot and day.

--- database/db.js
+++ database/db.js
@@ -14,16 +14,19 @@ var DjBlurbSchema = new Schema({
 	link: String,
 	timeslot: String,
 	day: String
-});
+}, {collection: 'DjBlurbs'});
 
 var DjBlurbModel = mongoose.model('DjBlurb', DjBlurbSchema);
 
-db.getABlurb = function(time) {
-	DjBlurbModel.find({timeslot: '10pm', day: 'Wed'}, 'link', function(err, blurb) {
-		if (err)
-			console.log('err');
-		console.dir(blurb);
+//expects a string like '10pm', '8am', '6pm'
+//i.e. hour and then am or pm. not 24-hour format
+//expects day string like 'Wed', 'Thu'
+//i.e. first three letters of the day, first letter capitalized
+db.getBlurbByTimeslotAndDay = function(time, day) {
+	DjBlurbModel.findOne({timeslot: time, day: day}, function(err, blurb) {
+		if (err) console.log('err');
+		return blurb;
 	});
-});
+};
 
 module.exports = db;
