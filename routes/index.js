57d73c5ebdfcae5e2bec45e694332b2f3bcd7c4e
MatteoVH <matteovh@ucla.edu>
2015-04-24 16:38:53 -0700

    Added logic to get the current time and day to pass to a db function. The db function will now return blurb data that is passed to the homepage view.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,28 @@ var router = express.Router();
 var db = require('../database/db');
 
 router.get('/', function(req, res) {
-	console.log(db.getAllBlurbs());
-	res.render('index');
+	var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	var date = new Date();
+
+	var day = days[date.getDay()];
+	var time = date.getHours();
+
+	//Change the time into the format our db is expecting
+	//AKA 12pm, 10am, 1pm: hour followed by am or pm
+	if (time === 0) {
+		time = '12am';
+	} else if (time < 12) {
+		time += 'am';
+	} else if (time == 12) {
+		time = '12pm';
+	} else {
+		time -= 12;
+		time += 'pm';
+	}
+	
+	db.getBlurbByTimeslotAndDay(time, day, function(err, blurb) {
+		res.render('index', blurb);
+	});
 });
 
 module.exports = router;
