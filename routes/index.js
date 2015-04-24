e1c6edd898701f2cf5bcbd6ae63e61d1cdf2ba6b
MatteoVH <matteovh@ucla.edu>
2015-04-24 14:39:32 -0700

    Fixed database function merge conflict.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../database/db');
 
 router.get('/', function(req, res) {
+	console.log(db.getAllBlurbs());
 	res.render('index');
 });
 
