9549a4a84ac6f47d447840178bea57236f75eeed
MatteoVH <matteovh@ucla.edu>
2015-04-24 17:37:17 -0700

    Added preliminary new blurb submission form.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var newBlurb = require('./routes/newBlurb');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/newBlurb', newBlurb);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
