d94830db9302bee1533f0eb65761f5d6fd0401fa
MatteoVH <matteovh@ucla.edu>
2015-05-12 16:51:20 -0700

    Now saving files to disk instead of keeping in memory.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(multer({ dest: './public/blurbImages/', inMemory: true}));
+app.use(multer({ dest: './public/blurbImages/'}));
 
 app.use('/', routes);
 app.use('/newBlurb', newBlurb);
