99956d75d6690f21f30851ee7d7f035d74be0762
MatteoVH <matteovh@ucla.edu>
2015-05-15 14:32:00 -0700

    Reset scripts file to working state.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,4 @@
-function injectStyles(rule) {
-  var div = $("<div />", {
-    html: '&shy;<style>' + rule + '</style>'
-  }).appendTo("body");    
-}
-
-
-// given the time of day, returns a random color scheme 
+// given the time of day, returns a random color scheme
 // matching it.
 function getRandColorScheme(timeOfDay) {
   var possColors, choice;
@@ -17,13 +10,13 @@ function getRandColorScheme(timeOfDay) {
       }
     case 'morning': // oranges, yellows, reds, greens
       {
-        possColors = ['Reds', 'Oranges', 'YlOrBr', 'YlOrRd', 'OrRd', 
+        possColors = ['Reds', 'Oranges', 'YlOrBr', 'YlOrRd', 'OrRd',
                    'PuRd', 'RdPu'];
         break;
       }
     case 'noon': // greens, purples, blues, yellows
       {
-        possColors = ['Greens', 'BrBG', 'PuOr', 
+        possColors = ['Greens', 'BrBG', 'PuOr',
                    'Spectral', 'YlGn'];
         break;
       }
@@ -41,14 +34,14 @@ function getRandColorScheme(timeOfDay) {
 function getColorSchemeFromTime() {
   var hour = new Date().getUTCHours();
 
-  // UCLA is located 7 hours behind UTC. Use UTC time so our 
+  // UCLA is located 7 hours behind UTC. Use UTC time so our
   // background colors will be synchronized wherever you are in the world.
   var timeOfDay = null;
 
   // Nighttime: 9pm - 6am == 21 - 6 PDT == 4 - 13 UTC
   if (hour >= 4 && hour < 13) {
-    return getRandColorScheme('morning');
-  } 
+    return getRandColorScheme('night');
+  }
   // Morning: 6am - 9am == 6-9 PDT == 13-16 UTC
   else if (hour >= 13 && hour < 16) {
     return getRandColorScheme('morning');
@@ -62,32 +55,10 @@ function getColorSchemeFromTime() {
     return getRandColorScheme('evening');
   }
 }
-
 function setButtons(darkColor, mediumColor, lightColor, midnight) {
-  injectStyles('.rc-active {background-color:'+ lightColor +';}');
-  injectStyles('.cbp-contentslider nav a:last-child {box-shadow: 1px 0 ' + lightColor + ';}')
-
-
-
-  $(".cbp-contentslider nav a").hover(function(){
-    $(this).css("background-color", lightColor);
-    $(this).css("color", "white");
-  }, function(){
-    $(this).css("background-color", "");
-    $(this).css("color", "black")
-  });
   $(".pop-button").css('background-color', lightColor);
-  $(".cbp-contentslider").css("border", "4px solid" + lightColor);
-  $(".cbp-contentslider nav a").css("border-right", "4px solid" + lightColor);
-  document.getElementById('#2').style.border = "none";
-  $(".cbp-contentslider h3").css("border-bottom", "4px solid" + lightColor);
-  $("nav").css("border-top", "4px solid" + lightColor);
-  injectStyles('.slick-prev:before {color: ' + lightColor + ' !important;');
-  injectStyles('.slick-next:before {color: ' + lightColor + ' !important;');
 
-
-
-  var boxShadowHoverCss = "1px 0px " + mediumColor + 
+  var boxShadowHoverCss = "1px 0px " + mediumColor +
     ", 0px 1px " + darkColor +
     ", 2px 1px " + mediumColor +
     ", 1px 2px " + darkColor +
@@ -97,7 +68,7 @@ function setButtons(darkColor, mediumColor, lightColor, midnight) {
     ", 3px 4px " + darkColor;
   //console.log(boxShadowHoverCss);
 
-  var boxShadowActiveCss = "1px 0px " + mediumColor + 
+  var boxShadowActiveCss = "1px 0px " + mediumColor +
     ", 0px 1px " + darkColor +
     ", 2px 1px " + mediumColor +
     ", 1px 2px " + darkColor;
@@ -142,12 +113,13 @@ function setPageTheme(colorScheme) {
   var medium = null;
   var light = null;
 
+  document.body.setAttribute('style', 'background-image: url(http://i.imgur.com/7vdDtPN.jpg); background-attachment: fixed;');
   if (colorScheme === "Greys") { // midnight space theme
     midnight = true;
     var filename = 'img/nebula' + Math.floor(Math.random()*3) + '.jpg';
     var backgroundAttribute = "background: url(" + filename +
       ") no-repeat center center fixed";
-    document.body.setAttribute('style', backgroundAttribute);
+    //document.body.setAttribute('style', backgroundAttribute);
     dark = '#292e49';
     medium = '#2d4b56';
     light = '#30716d';
@@ -156,8 +128,8 @@ function setPageTheme(colorScheme) {
     var colors = Trianglify.colorbrewer[colorScheme][9];
     var t = new Trianglify({noiseIntensity: 0.0, cellsize: 175, x_gradient: colors});
     var pattern = t.generate(4000, 3000);
-    document.body.setAttribute('style', 'background: ' + 
-        pattern.dataUrl + ' no-repeat center center fixed');
+    //document.body.setAttribute('style', 'background: ' +
+        //pattern.dataUrl + ' no-repeat center center fixed');
 
     dark = colors[8];
     medium = colors[7];
@@ -212,7 +184,7 @@ $(document).ready(function() {
   // Generate that background image!
   var colorScheme = getColorSchemeFromTime();
   //debug
-  setPageTheme(colorScheme); 
+  setPageTheme(colorScheme);
 
   checkCarouselData();
 });
@@ -223,7 +195,5 @@ function checkCarouselData() {
     checkCarouselData();
   }, 30000);
 }
-
-
 
 
