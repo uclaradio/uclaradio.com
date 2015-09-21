93fac0554b759d58fcdcb0da4625f419d2b48769
MatteoVH <matteovh@ucla.edu>
2015-09-21 13:32:57 -0700

    temporarily remove rush page from site

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var ios = require('./routes/ios');
 var schedule = require('./routes/schedule');
 var pages = require('./routes/pages');
 var shows = require('./routes/shows');
-var rush = require('./routes/rush');
 var manageShows = require('./routes/manageShows');
 
 
@@ -41,7 +40,6 @@ app.use('/ios', ios);
 app.use('/schedule', schedule);
 app.use('/pages', pages);
 app.use('/shows', shows);
-app.use('/rush', rush);
 app.use('/manageShows', manageShows);
 
 // catch 404 and forward to error handler
