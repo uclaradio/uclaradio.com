8d29e270b1b76292f9852c80b7b56c1d9df765cb
Chris Laganiere <ChrisLaganiere@gmail.com>
2016-01-19 17:42:09 -0800

    Revert "fix merge conflicts"
    This reverts commit 6ff661fc5719ae731eb35b0a77b311d95c0a900f, reversing
    changes made to 1f89f82c56288f30c3abf23d75f1348e13397445.

--- routes/managers.js
+++ routes/managers.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../database/db');
-var helper_funcs = require('./helper_funcs.js');
 
 router.get('/', function(req, res) {
-	
-	db.getAllManagers(function(err, managers) {
-
-		// sort by position name
-		managers.sort(helper_funcs.sort_by('position', false, false));
-
-		res.render('managers', {managers: managers});
-	});
+	res.render('managers');
 });
 
 module.exports = router;
