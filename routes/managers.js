4e51b4ffcbd082ce5678be2aa09e5adc71500e66
Chris Laganiere <ChrisLaganiere@gmail.com>
2016-01-19 17:39:41 -0800

    fix merge conflicts

--- routes/managers.js
+++ routes/managers.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../database/db');
+var helper_funcs = require('./helper_funcs.js');
 
 router.get('/', function(req, res) {
-	res.render('managers');
+	
+	db.getAllManagers(function(err, managers) {
+
+		// sort by position name
+		managers.sort(helper_funcs.sort_by('position', false, false));
+
+		res.render('managers', {managers: managers});
+	});
 });
 
 module.exports = router;
