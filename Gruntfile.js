ea0045063f80f40072a84b3243bdae43d3076be6
Chris Laganiere <ChrisLaganiere@gmail.com>
2016-03-22 18:39:41 -0700

    set up automatic compiling of files in react/ directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
 	var webpack = require('webpack');
+	var glob = require("glob");
 
 	// We define two entries. One for our application and one for vendors
 	// var entry = { main: './react/panel.js', vendors: ['react', 'elemental'] };
@@ -10,60 +11,46 @@ module.exports = function(grunt) {
 	// We need to uglify that code on deploy
 	var uglifyPlugin = [new webpack.optimize.UglifyJsPlugin()];
 
+	// Directory where *.js and *.jsx files will be compiled and placed in ./public/build
+	var directory = 'react';
+	// go through files in this directory and add them to target entry
+ 	var files = grunt.file.expand({cwd: directory}, '*.js', '*.jsx');
+ 	var entry = {vendors: ['react', 'elemental']};
+ 	for (var i = 0; i < files.length; i++) {
+ 		var filename = files[i].replace(/.js[x]?/g, '');
+ 		entry[filename] = './' + directory + '/' + files[i];
+ 	}
+
+	console.log("entry:", entry);
+
 	// The module options takes loaders, in this case transforming JSX to normal
 	// javascript
 	var module = { loaders: [{ test: /\.js[x]?$/, loader: 'jsx' }, { test: /\.less$/, loader: 'style!css!less' }] };
 	grunt.initConfig({
 	  webpack: {
-	  	panel: {
-	      entry: { main: './react/panel.js', vendors: ['react', 'elemental'] },
+	  	build: {
+	      entry: entry,
 	      plugins: commonPlugin.concat(uglifyPlugin),
 	      stats: {
 	        timings: true
 	      },
-	      // files: [{
-       //      expand: true,
-       //      cwd: 'react',
-       //      src: '*.js',
-       //      dest: 'public/build',
-       //      ext: '.min.js'
-       //    }],
 	      output: {
-	        filename: 'panel.js',
+	        filename: '[name].min.js',
 	        path: './public/build'
 	      },
 	      module: module
 	    },
-
-	  	managerPanel: {
-	      entry: { main: './react/managerPanel.js', vendors: ['react', 'elemental'] },
-	      plugins: commonPlugin.concat(uglifyPlugin),
-	      stats: {
-	        timings: true
-	      },
-	      output: {
-	        filename: 'managerPanel.js',
-	        path: './public/build'
-	      },
-	      module: module
-	    }
 	  }
 	});
 
 	grunt.loadNpmTasks('grunt-webpack');
-	// build all
-	grunt.registerTask('build', ['webpack:panel', 'webpack:managerPanel']);
-	// panel (keep alive)
-	grunt.registerTask('panel', 'Build panel with keepalive', function () {
-		grunt.config.set('webpack.panel.keepalive', true);
-		grunt.config.set('webpack.panel.watch', true);
-	  	grunt.task.run('webpack:panel');
-	});
-	// managerPanel (keep alive)
-	grunt.registerTask('managerPanel', 'Build managerPanel with keepalive', function () {
-		grunt.config.set('webpack.managerPanel.keepalive', true);
-		grunt.config.set('webpack.managerPanel.watch', true);
-	  	grunt.task.run('webpack:managerPanel');
+	// build all files in ./react
+	grunt.registerTask('build', ['webpack:build']);
+	// 'watch' task (keep alive)
+	grunt.registerTask('watch', 'Build all files on change', function () {
+		grunt.config.set('webpack.build.keepalive', true);
+		grunt.config.set('webpack.build.watch', true);
+	  	grunt.task.run('webpack:build');
 	});
 	// default ('grunt')
 	grunt.registerTask('default', ['build']);
