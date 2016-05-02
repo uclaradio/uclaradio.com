cd0d1e57f5447fc5e365cc597fce8ae911d616a6
Chris Laganiere <ChrisLaganiere@gmail.com>
2016-05-02 12:27:16 -0700

    fix setup script

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@ echo setting up UCLARadio repository...
 brew update
 brew install node
 brew install mongodb
+# storage for mongodb
 sudo mkdir -p /data/db
+# privileges for mongodb
 sudo chown -R $USER /data/db
 npm install
-
+echo {\"secretpassword\": \"stillasecret\"} > passwords.json
 echo finished.
