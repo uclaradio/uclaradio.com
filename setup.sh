2e99983156126ae4332b42dbc4c437de8adc1b95
Chris Laganiere <ChrisLaganiere@gmail.com>
2016-09-26 00:12:47 -0700

    fix setup script

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ sudo mkdir -p /data/db
 # privileges for mongodb
 sudo chown -R $USER /data/db
 npm install
-cp examplePassword.json passwords.json
+cp defaultPasswords.json passwords.json
+node database/scripts/setupPanel.js
 echo finished.
