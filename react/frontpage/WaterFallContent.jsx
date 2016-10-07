9fc109758b8ed98327a7b3f11fdc6da9def97088
hsuregan <hsuregan@gmail.com>
2016-10-06 21:09:36 -0700

    add date to each of the tiles

--- react/frontpage/WaterFallContent.jsx
+++ react/frontpage/WaterFallContent.jsx
@@ -2,6 +2,7 @@
 
 var React = require('react');
 var passwords = require('../../passwords.json');
+var Dates = require('../misc/Dates.js');
 var numberOfFBPosts = 7;
 var FB = "https://graph.facebook.com/uclaradio?fields=posts.limit("+numberOfFBPosts+"){full_picture,message,created_time,link}&access_token=" + passwords["FB_API_KEY"];
 var WaterFallContent = React.createClass({
@@ -31,6 +32,10 @@ var WaterFallContent = React.createClass({
 									!el['message'].includes("http") && 
 									<div className='waterfall-box-content-text'>
 										{
+											<div className='waterfall-box-content-text-date'>{formateDate(el['created_time'])}</div> 
+											// el['message']
+										}
+										{
 											el['message']
 										}
 									</div>
@@ -50,4 +55,8 @@ var containsHttp = function(myString) {
 	return myString.split(" ").map(function(el) { return el.includes("http") ? "click" : el }).join(' ');
 }
 
+var formateDate = function(dateString) {
+	var date = new Date(dateString);
+	return Dates.availableDays[date.getDay()] + ", " +date.getMonth() + "/" + date.getDate();
+}
 module.exports = WaterFallContent;
