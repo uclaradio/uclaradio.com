a56681c41279be26056bec4e58ca0c77c48420da
Chris Laganiere <ChrisLaganiere@gmail.com>
2016-11-20 19:19:02 -0800

    code review

--- react/frontpage/DJInfo.jsx
+++ react/frontpage/DJInfo.jsx
@@ -1,26 +1,27 @@
 // DJInfo.jsx
 var React = require('react');
-var defaultDJPic = "/img/radio.png"
-var baseURL = "https://uclaradio.com";
+var defaultDJPic = "/img/bear_transparent.png"
 
 // Common Elements
 var RectImage = require('../common/RectImage.jsx');
 
-
+/*
+DJInfo: UI element showing information for a dj
+@prop picture: url for dj profile picture
+@prop name: dj name
+*/
 var DJInfo = React.createClass({
     getDJImage: function(picURL) {
-        if (!picURL) {
-            return defaultDJPic;
+        if (picURL) {
+            return picURL;
         }
-        else {
-            return baseURL+picURL;
-        }
+        return defaultDJPic;
     },
     render: function() {
     	return (
-    		<div className="dj-tile">
+    		<div className="djTile">
     			<RectImage maxWidth="200px" src={this.getDJImage(this.props.picture)} circle />
-    			<div className="dj-tile-info">
+    			<div className="djTileInfo">
     				<h4>{this.props.name}</h4>
     			</div>
     		</div>
--- react/frontpage/DJList.jsx
+++ react/frontpage/DJList.jsx
@@ -1,9 +1,14 @@
 // DJList.jsx
+
 var React = require('react');
 
 var DJInfo = require('./DJInfo.jsx');
+
 var DJUrl = "/api/djs";
 
+/*
+DJList: fetches a json list of djs from API and displays data
+*/
 var DJList = React.createClass({
     getInitialState: function() {
         return {
@@ -13,20 +18,21 @@ var DJList = React.createClass({
     componentWillMount: function() {
         this.serverRequest = $.get(DJUrl, function(result) {
             this.setState({
-                djs:data
+                djs:result.djs
             });
         }.bind(this));
     }, 
     render: function() {
         var djs = this.state.djs.map(function(dj){
-            return <DJInfo name={dj.djName ? dj.djName : dj.fullName} picture={dj.picture} />
+            return <DJInfo name={dj.djName ? dj.djName : dj.fullName} picture={dj.picture} key={dj.id} />
         });
 
         return (
-            <div>
-            {djs}
+            <div className="djList">
+                {djs}
             </div>
         );
     } 
 });
+
 module.exports = DJList;
