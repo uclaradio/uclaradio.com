abd0604b93cbfc05c535dc507771027bde1974c3
Chris Laganiere <ChrisLaganiere@gmail.com>
2016-11-22 15:06:01 -0800

    cleanup waterfall restructure

--- react/frontpage/WaterFallContent.jsx
+++ react/frontpage/WaterFallContent.jsx
@@ -21,24 +21,40 @@ var WaterFallContent = React.createClass({
     };
   },
   componentDidMount: function() {
+    // Get initial data
     $.get(SocialMediaURL, function (result) {
       this.setState({
         paginatedDataInProgress: false,
-        // socialMediaPosts:result['social_media'],
         fb_pagination_until:result['fb_pagination_until']
       }, function () {
         waterfall = new Waterfall({ minBoxWidth: 250 });
-        result['social_media'].map(function(el) {
-          var boxHandle = nodeFromPost(el);
-          waterfall.addBox(boxHandle);
-        });
+        this.appendPosts(result['social_media']);
         window.addEventListener('scroll', this.handleScroll);
       });
     }.bind(this));
-    
   },
   componentWillUnmount: function() {
     window.removeEventListener('scroll', this.handleScroll);
+  },
+  // fetch another page of posts, display in waterfall
+  fetchMorePosts: function() {
+    $.post(getMoreFBPostsURL, {
+      until: this.state.fb_pagination_until
+    },function (result) {
+      this.setState({
+        fb_pagination_until: result['fb_pagination_until'],
+        paginatedDataInProgress: false
+      }, function() {
+        this.appendPosts(result['social_media'])
+      });
+    }.bind(this));
+  },
+  // Add post objects from API to waterfall content as DOM elements
+  appendPosts: function(newPosts) {
+    newPosts.map(function(el) {
+      var boxHandle = nodeFromPost(el);
+      waterfall.addBox(boxHandle);
+    });
   },
   handleScroll: function(event) {
     var i = waterfall.getHighestIndex();
@@ -47,26 +63,12 @@ var WaterFallContent = React.createClass({
       var lastBox = Array.prototype.slice.call(waterfall.columns[i].children, -1)[0];
       if (checkSlide(lastBox)) {
         if (this.state.paginatedDataInProgress == false) {
-          // Locking mechanism so scrolling won't cause an infinite amt of requests
+          // locking mechanism so scrolling won't cause an infinite amt of requests
           this.setState({
             paginatedDataInProgress: true
           });
-          //request next set of Facebook posts
-          console.log("$handleScroll");
-          this.serverRequest = $.post(getMoreFBPostsURL, {
-            until: this.state.fb_pagination_until
-          },function (result) {
-            result['social_media'].map(function(el) {
-              var boxHandle = nodeFromPost(el);
-              waterfall.addBox(boxHandle);
-            });
-            // var newPosts = this.state.socialMediaPosts.concat(result['social_media']);
-            this.setState({
-              // socialMediaPosts: newPosts,
-              fb_pagination_until: result['fb_pagination_until'],
-              paginatedDataInProgress: false
-            });
-          }.bind(this));
+          // request next set of Facebook posts
+          this.fetchMorePosts();
         }
       }
     }
@@ -81,7 +83,7 @@ var WaterFallContent = React.createClass({
 
 
 function checkSlide(elem) {
-  if(elem) {
+  if (elem) {
     var screenHeight = (document.documentElement.scrollTop || document.body.scrollTop)
       + (document.documentElement.clientHeight || document.body.clientHeight);
     var elemCenter = elem.offsetTop + elem.offsetHeight / 2;
@@ -89,19 +91,23 @@ function checkSlide(elem) {
   }
 }
 
+// create new DOM element from a tumblr or facebook post json object
 function nodeFromPost(post) {
   var link = post['link'] || post['post_url'];
   var picture = post['full_picture'];
   var summary = post['summary'] || post['message'];
-  var created = post['created_time'];
-  return newNode(picture, summary, created, link);
+  var created = formatDate(post['created_time']);
+  var platform = post['platform'] || "FB";
+  return newNode(picture, summary, created, link, platform);
 }
 
-function newNode(full_picture, summary, created_time, link) {  
+// create new DOM element representing post with provided content
+function newNode(full_picture, summary, created_time, link, platform) {  
   var box = document.createElement('div');
   box.className = 'wf-box';
   var a = document.createElement('a');
   a.href = link;
+  a.target = "_blank";
   var box_content = document.createElement('div');
   box_content.className = 'wf-box-content';
   if (full_picture) {
@@ -110,12 +116,16 @@ function newNode(full_picture, summary, created_time, link) {
     box_content.appendChild(image);
   }
   var box_content_text = document.createElement('div');
-      box_content_text.className = 'wf-box-content-text';
-  var box_content_date = document.createElement('div');
-     box_content_date.className = 'wf-box-content-text-date';
-     created_time = String(created_time);
-     box_content_date.appendChild(document.createTextNode(created_time));
-  box_content_text.appendChild(box_content_date);
+  if (platform === "TUMBLR") {
+    box_content_text.className = 'wf-box-content-blog';
+  } else {
+    box_content_text.className = 'wf-box-content-text';
+    var box_content_date = document.createElement('div');
+    box_content_date.className = 'wf-box-content-text-date';
+    created_time = String(created_time);
+    box_content_date.appendChild(document.createTextNode(created_time));
+    box_content_text.appendChild(box_content_date);
+  }
   box_content_text.appendChild(document.createTextNode(summary));
   summary = String(summary);
   if (!summary.includes("http")) {
