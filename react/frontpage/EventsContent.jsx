6378cedf4978b62f8d0a3fc8f44e6d96feba0be1
kianadk <kianadkavoosi@gmail.com>
2016-12-15 13:37:57 -0800

    switched to new design

--- react/frontpage/EventsContent.jsx
+++ react/frontpage/EventsContent.jsx
@@ -8,73 +8,92 @@ var Button = require('react-bootstrap').Button;
 
 //content of events page
 var EventsContent = React.createClass({
-	getInitialState: function(){
-		return{
-			currentShow: null
-		};
-	},
-	showModal: function(e){
+	goToEventPage: function(e){
 		var target = e.target
-		while(target.className != "event" && target !== document)
+		while(target.className != "event" && target.className !== wf-container)
 			target = target.parentNode;
-		if(target !== document)
-			this.setState({currentShow: target.getElementsByClassName("bandName")[0].innerHTML});
-	},
-	hideModal: function(){
-		this.setState({currentShow: null})
+		if(target.className !== wf-container)
+			window.open("https://www.facebook.com/UCLARadio/");
 	},
 	render: function() {
 		return(
-			<div className='wf-container' onClick={this.showModal}>
-				{JSON.parse(this.props.data).events.map (function(el) {
-						return (
-							<div className="monthEvents" key={el['month']}>
-								<h1>{el['month']}</h1>
-								<div className="eventBlock">
-									{el['arr'].map( function(event){
-										return (
-										<div className="event" key={event['id']}>
-											<p className="bandName">{getBandName(event['summary'])}</p>
-											<img src="https://avatars3.githubusercontent.com/u/7256558?v=3&s=466" className="bandPic"/>
-											<div className="overlay">
-												<div className="eventOverlay">
-													<p className="showDate">{formatDate(event['start'])}</p>
-													<p className="separator">... ... ...</p>
-													<p className="venue">{getVenue(event['summary'])}</p>
+			<div>
+				<div className="colorKey">
+					<div className="colorKeyLabel">
+						<p>Giveaway</p>
+						<div className="dot" style={{backgroundColor: "#3c84cc"}}></div>
+					</div>
+					<div className="colorKeyLabel">
+						<p>UCLA Radio Presents</p>
+						<div className="dot" style={{backgroundColor: "#098440"}}></div>
+					</div>
+					<div className="colorKeyLabel">
+						<p>Campus Event</p>
+						<div className="dot" style={{backgroundColor: "#842b78"}}></div>
+					</div>
+					<div className="colorKeyLabel">
+						<p>Local Event</p>
+						<div className="dot" style={{backgroundColor: "#cca437"}}></div>
+					</div>
+				</div>
+				<div className='wf-container' onClick={this.goToEventPage}>
+					{JSON.parse(this.props.data).events.map (function(el) {
+							return (
+								<div className="monthEvents" key={el['month']}>
+									<h1>{el['month']}</h1>
+									<div className="eventBlock">
+										{el['arr'].map( function(event){
+											return (
+											<div className="event" key={event['id']}>
+												<img src={event['image']} className="bandPic"/>
+												<div className="overlayWrapper">
+													<div className="overlay" style={{backgroundColor: getBackgroundColor(event['type'])}}>
+														<p className="eventDate">{formatDate(event['start'])}</p>
+														<div className="eventOverlay">
+															<p className="bandName">{getBandName(event['summary'])}</p>
+															<p className="separator">. . .&nbsp;&nbsp;&nbsp;. . .&nbsp;&nbsp;&nbsp;. . .</p>
+															<p className="venue">{getVenue(event['summary'])}</p>
+														</div>
+													</div>
+													<div className="hoverOverlay">
+														<p className="enterLabel">click for more details</p>
+													</div>
 												</div>
-												<p className="enterLabel">(click to enter contest)</p>
-											</div>
-										</div>											
-										);
-									})
-									}
+											</div>											
+											);
+										})
+										}
+									</div>
 								</div>
-							</div>
-						);
-					})
-				}
-				<Modal show={this.state.currentShow} onHide={this.hideModal} className="enterContest">
-			<Modal.Body style={{marginTop: 20}}>
-				<p className="showLabel">SHOW: </p><p className="showDesc">{this.state.currentShow}</p>
-				<form>
-					<div className="formGroup">
-						<p className="formLabel">ENTER NAME: </p>
-						<input type="text" label="name" className="formInput"/><br/>
-					</div>
-					<div className="formGroup">
-						<p className="formLabel">ENTER EMAIL: </p>
-						<input type="text" label="email" className="formInput"/><br/>
-					</div>
-					<Button className="formButton">
-						SUBMIT
-					</Button>
-				</form>
-			</Modal.Body>
-				</Modal>
+							);
+						})
+					}
+				</div>
 			</div>
 		);
 	}
 });
+
+var getBackgroundColor = function(type){
+	var color;
+	switch(type){
+		case "Giveaway":
+			color = "#3c84cc";	//blue
+			break;
+		case "UCLA Radio Presents":
+			color = "#098440";	//green
+			break;
+		case "Campus Event":
+			color = "#842b78";	//magenta
+			break;
+		case "Local Event":
+			color = "#cca437";	//orange
+			break;
+		default:
+			color = "grey";
+	}
+	return color;
+};
 
 var getBandName = function(desc){
 	return desc.substring(0, desc.indexOf("@") - 1);
@@ -86,17 +105,7 @@ var getVenue = function(desc){
 
 var formatDate = function(dateString) {
     var date = new Date(dateString);
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var dq = "th";
-    if(date.getDate() % 10 == 1)
-    	dq = "st";
-    else if (date.getDate() % 10 == 2)
-    	dq = "nd";
-    else if (date.getDate() % 10 == 3)
-    	dq = "rd";
-    if(date.getDate > 9 && date.getDate < 20)
-    	dq = "th";
-	return months[date.getMonth()] + " " + date.getDate() + dq;
+	return date.getDate();
 };
 
 module.exports = EventsContent;
