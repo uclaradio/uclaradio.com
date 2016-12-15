674cf34c0eb7d7eea7a67fc1880175421e3da96f
Chris <chris@Danielles-MacBook-Pro.local>
2016-12-14 18:12:49 -0800

    remove sotm november

--- setup.sh
+++ setup.sh
@@ -1,13 +1,27 @@
 #! /bin/bash
-echo setting up UCLARadio repository...
+
+# Set up steps for macOS
+# needs to have homebrew installed already
+
+echo 'Setting up UCLARadio repository...'
+echo 'installing macOS packages'
 brew update
 brew install node
 brew install mongodb
+
 # storage for mongodb
 sudo mkdir -p /data/db
 # privileges for mongodb
 sudo chown -R $USER /data/db
-npm install
+
+echo 'installing npm packages'
+## note: might need to update if 'node-gyp rebuild' fails
+# npm explore npm -g -- npm install node-gyp@latest
+sudo npm install
+
 cp defaultPasswords.json passwords.json
+
+echo 'seeding database'
 node database/scripts/setupPanel.js
-echo finished.
+
+echo 'Finished.'
