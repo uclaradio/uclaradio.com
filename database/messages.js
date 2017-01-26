648549fe706a96a8a30a191aef622bbb83544cbc
chrislaganiere <chrislaganiere@gmail.com>
2017-01-26 00:24:43 +0000

    guarantee unique usernamesby increasing digits when conflicts discovered

--- database/messages.js
+++ database/messages.js
@@ -10,11 +10,14 @@ var MessageSchema = new mongoose.Schema({
 	id: { type: Number, index: true },
 	text: String,
   	user: { type: String, index: true },
-  	reported: {type: Boolean, default: false},
+  	reported: {type: Boolean, default: false, index: true},
   	date: { type: Date, default: Date.now }
 });
 
 var MessageModel = mongoose.model('messages', MessageSchema);
+
+// number of times script should attempt and check to confirm a unique username, per round
+var uniqueCheckIterations = 10;
 
 // create a new message object with given text, username, and a unique id
 messages.saveMessage = function(data, callback) {
@@ -72,31 +75,35 @@ messages.next = function(messageID, volume, callback) {
 	})
 }
 
-// supply a (hopefully unique) username
+// supply a username
 messages.generateUsername = function(callback) {
-	attemptUniqueUsername(10, callback);
+	// generate a unique with >3 digits: "Guest479"
+	attemptUniqueUsername(uniqueCheckIterations, 3, callback);
 }
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomUsername() {
-	return "Guest" + getRandomInt(0, 99999);
+// generate a random username with some max number of digits
+// example: digits <= 5: "Guest430" or "Guest47988" could be generated
+function randomUsername(digits) {
+	var maxInt = Math.pow(10, digits) - 1;
+	return "Guest" + getRandomInt(0, maxInt);
 }
 
 // try and get a random username by checking that no identical user exists in database.
 // after some number of attempts, give up and reuse a username
-function attemptUniqueUsername(iterations, callback) {
-	var random = randomUsername();
+function attemptUniqueUsername(iterations, digits, callback) {
+	var random = randomUsername(digits);
 	if (iterations < 1) {
-		callback(random);
+		attemptUniqueUsername(uniqueCheckIterations, digits+1, callback);
 		return;
 	}
 	MessageModel.findOne({user: random}, function(err, data) {
 		if (data) {
 			// username taken, try again
-			attemptUniqueUsername(iterations-1, callback);
+			attemptUniqueUsername(iterations-1, digits, callback);
 		} else {
 			// found unique username
 			callback(random);
