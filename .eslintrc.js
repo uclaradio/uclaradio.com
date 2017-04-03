4390009c63bbec5987ef648442ae9d5b3cd9179a
Nathan Smith <nathan@nathansmith.io>
2017-04-02 20:40:17 -0700

    Fix all scss

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
-
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true,
+  },
   "parserOptions": {
     "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
       "jsx": true
-    }
+    },
   },
   "plugins": ["react"],
   "rules": {
