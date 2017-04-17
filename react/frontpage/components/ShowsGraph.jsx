ddd3a7bde04fef44c0c46cb2985aecf6668a0f6b
Nicolas Mockovciak <nmockovciak@ucla.edu>
2017-04-16 17:11:32 -0700

    Fixed bug. Grid view supports late night shows now.

--- react/frontpage/components/ShowsGraph.jsx
+++ react/frontpage/components/ShowsGraph.jsx
@@ -40,9 +40,9 @@ const ShowsGraph = React.createClass({
     });
   },
   
-  findStartTime: function() { // find earliest show time
+  findStartTime: function() { // find earliest show time (after 5am)
     var found = 0;
-    for(var s = 1; s < Dates.availableTimes.length; s++){
+    for(var s = 5; s < Dates.availableTimes.length; s++){
       for(var showIndex = 0; showIndex < this.props.shows.length; showIndex++){
         if(this.props.shows[showIndex].time == Dates.availableTimes[s]){
           found = 1;
@@ -56,6 +56,20 @@ const ShowsGraph = React.createClass({
 
   findEndTime: function() {  // find latest show time
     var found = 0;
+
+    // check late night (after midnight and before 5am) shows first
+    for(var e = 4; e >= 0; e--){
+      for(var showIndex = 0; showIndex < this.props.shows.length; showIndex++){
+        if(this.props.shows[showIndex].time == Dates.availableTimes[e]){
+          found = 1;
+          break;
+        }
+      }
+      if(found) {break;}
+    }
+    if (found) { return e;}
+
+    // now check before midnight
     for(var e = Dates.availableTimes.length-1; e > 0; e--){
       for(var showIndex = 0; showIndex < this.props.shows.length; showIndex++){
         if(this.props.shows[showIndex].time == Dates.availableTimes[e]){
@@ -84,7 +98,11 @@ const ShowsGraph = React.createClass({
     var start = this.findStartTime();
     var end = this.findEndTime(); 
 
-    for (var hour = start; hour < end+1; hour++) {
+    var hour = start;
+
+    var endMargin = end < 5 ? Dates.availableTimes.length-1 : end;
+
+    for (; hour < endMargin+1; hour++) {
       var hourString = Dates.availableTimes[hour];
       showBlocks.push( 
         <div className="hourBlocks">
@@ -102,6 +120,54 @@ const ShowsGraph = React.createClass({
         </div>
       );
     }
+
+    if (end < 5) {
+      for (hour = 0; hour < end+1; hour++) {
+        var hourString = Dates.availableTimes[hour];
+        showBlocks.push( 
+          <div className="hourBlocks">
+            <p className="timeStyle" style={{width: dayWidth}}>{hourString}</p>
+            { week.map((day) => {
+              var displayDay;
+
+              switch (day) {
+                case "sunday":
+                  displayDay = "monday";
+                  break;
+                case "monday":
+                  displayDay = "tuesday";
+                  break;
+                case "tuesday":
+                  displayDay = "wednesday";
+                  break;
+                case "wednesday":
+                  displayDay = "thursday";
+                  break;
+                case "thursday":
+                  displayDay = "friday";
+                  break;
+                case "friday":
+                  displayDay = "saturday";
+                  break;
+                case "saturday":
+                  displayDay = "sunday";
+                  break;
+              }
+
+              var show = this.state.schedule && this.state.schedule[displayDay][hour];
+              return (
+                <ShowBlock isValidShow={(show && show.title)}
+                  isCurrentShow={show && show.id === this.props.currentShowID}
+                  isActiveShow={show && show.id === this.props.activeShowID}
+                  isSpotlightShow={show && show.id === this.props.spotlightShowID}
+                  handleClick={()=>{show && this.props.onShowClick(show)}} />
+              );
+            })}
+          </div>
+        );
+      }
+
+    } 
 
     return (
       <div className="showsGraph">
@@ -182,7 +248,7 @@ const sortedShows = (shows) => {
     var show = shows[s];
     var day = Dates.dayFromVar(show.day).toLowerCase();
     var hour = Dates.availableTimes.indexOf(show.time);
-    if (day && hour) {
+    if (day) {
       schedule[day][hour] = show;
     }
   }
