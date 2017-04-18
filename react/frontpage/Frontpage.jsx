0aedcf58d68c4a39b0ffcd68cbf956c3c7938ddc
binerys <bree.nery@gmail.com>
2017-04-18 08:11:19 -0700

    SoTW: Bruins Talking Toons

--- react/frontpage/Frontpage.jsx
+++ react/frontpage/Frontpage.jsx
@@ -164,8 +164,8 @@ const Frontpage = React.createClass({
     this.interval = setInterval(this.props.updateNowPlaying, 30*1000);
 
     // update now playing and fetch initial shows data
-    // 04/10/17 - Basscamp
-    this.props.setSpotlightShowID(235);
+    // 04/18/17 - Bruins Talking Toons
+    this.props.setSpotlightShowID(202);
     this.props.updateShows();
   },
   componentWillUnmount: function() {
