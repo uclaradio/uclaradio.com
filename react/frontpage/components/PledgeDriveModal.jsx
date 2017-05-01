c65d376389312701f429556a004ae19d10163b75
Nicolas Mockovciak <nmockovciak@ucla.edu>
2017-04-30 23:01:56 -0700

    Added popup image and link to pledge drive modal

--- react/frontpage/components/PledgeDriveModal.jsx
+++ react/frontpage/components/PledgeDriveModal.jsx
@@ -1,6 +1,13 @@
 const React = require('react');
 import { Modal, Button } from 'react-bootstrap';
 
+import  { Link } from 'react-router';
+
+const PledgeDrivePhoto = {
+  img: "/img/p-drive-modal.jpg",
+  link: "http://spark.ucla.edu/uclaradio"
+};
+
 const PledgeDriveModal = React.createClass({
   getInitialState() {
     return { showModal: true };
@@ -13,28 +20,13 @@ const PledgeDriveModal = React.createClass({
   render() {
     return (
       <Modal show={this.state.showModal} onHide={this.close}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
         <Modal.Body>
-          <h4>Text in a modal</h4>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-
-          <hr />
-
-          <h4>Overflowing text to show scroll behavior</h4>
-          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dpibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac cnsectetur ac, vestibulum at eros.</p>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vvamus sagittis lacus vel augue laoreet rutrum faucibus dolor actor.</p>
-          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Dnec ullamcorper nulla non metus auctor fringilla.</p>
-          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+          <Link to={PledgeDrivePhoto.link}>
+            <img src={PledgeDrivePhoto.img} style={{width: '100%'}}> </img>
+          </Link>  
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={this.close}>Close</Button>
+          <Button onClick={this.close}>Donate Later</Button>
         </Modal.Footer>
       </Modal>
     );
