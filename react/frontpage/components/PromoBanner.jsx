27330cde0815b6e98c6a067cc617d009cc071cc8
binerys <bree.nery@gmail.com>
2017-05-03 22:27:11 -0700

    SoTM: Lost in the Sauce

--- react/frontpage/components/PromoBanner.jsx
+++ react/frontpage/components/PromoBanner.jsx
@@ -16,11 +16,8 @@ require('./PromoBanner.scss');
 
 // Promo Banner Data 
 const bannerData = [
-  {"img": "/img/promo/sotm/sotm_april_2017.png", "link": "/shows/182"}
-  // Will update once their shows are updated
-  // {"img": "/img/sotm-mar17-nuindigo.png", "link": "/shows/90"},
-  // {"img": "/img/sotm-feb2017.jpg", "link": "/shows/75"},
-  // {"img": "/img/sotm_january_2017.png", "link": "/shows/83"}
+  {"img": "/img/promo/sotm/sotm-may2017.jpg", "link": "/shows/182"},
+  {"img": "/img/promo/sotm/sotm_april_2017.png", "link": "/shows/266"}
 ];
 
 var PromoBanner = React.createClass({
