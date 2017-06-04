bf27a41680180f09a8575a0a8c26ebd7b18249f4
jdliaw <jennifer.liaw07@gmail.com>
2017-06-04 00:41:53 -0700

    made marquee responsive

--- react/frontpage/components/StreamMarquee.jsx
+++ react/frontpage/components/StreamMarquee.jsx
@@ -1,12 +1,22 @@
+// StreamMarquee.jsx
+
 import React from 'react';
 
 require('./StreamMarquee.scss');
 
+/*
+StreamMarquee:
+
+@prop message: message to display on the marquee
+*/
+
 var StreamMarquee = React.createClass({
   render: function() {
     return (
-      <div className="marquee">
-        <p>{this.props.message}</p>
+      <div className="marquee-container">
+        <div className="marquee">
+          <p>{this.props.message}</p>
+        </div>
       </div>
     );
   }
