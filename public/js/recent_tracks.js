ed5cd4d522593ef5e3a911b6d8efa4860ae90ce1
Nathan Smith <nathan@nathansmith.io>
2017-08-13 14:29:37 -0700

    Update recent_tracks.js

--- public/js/recent_tracks.js
+++ public/js/recent_tracks.js
@@ -1,45 +1,39 @@
 function calculateDateAgo(secAgo) {
-  var agoString, agoRange, agoScaled;
+  let agoString, agoRange, agoScaled;
   if (secAgo >= (agoRange = 60 * 60 * 24))
-    agoString =
-      (agoScaled = Math.floor(secAgo / agoRange)) +
-      ' ' +
-      (agoScaled > 1 ? 'days' : 'day') +
-      ' ago';
+    agoString = `${(agoScaled = Math.floor(secAgo / agoRange))} ${agoScaled > 1
+      ? 'days'
+      : 'day'} ago`;
   else if (secAgo >= (agoRange = 60 * 60))
-    agoString =
-      (agoScaled = Math.floor(secAgo / agoRange)) +
-      ' ' +
-      (agoScaled > 1 ? 'hours' : 'hour') +
-      ' ago';
+    agoString = `${(agoScaled = Math.floor(secAgo / agoRange))} ${agoScaled > 1
+      ? 'hours'
+      : 'hour'} ago`;
   else if (secAgo >= (agoRange = 60))
-    agoString =
-      (agoScaled = Math.floor(secAgo / agoRange)) +
-      ' ' +
-      (agoScaled > 1 ? 'minutes' : 'minute') +
-      ' ago';
+    agoString = `${(agoScaled = Math.floor(secAgo / agoRange))} ${agoScaled > 1
+      ? 'minutes'
+      : 'minute'} ago`;
   else if (secAgo >= -60) agoString = 'listening just now';
   else agoString = 'soon ;)';
   return agoString;
 }
 
 function truncateName(name, l) {
-  return name.length > l ? name.substr(0, l - 2) + '\u2026' : name;
+  return name.length > l ? `${name.substr(0, l - 2)}\u2026` : name;
 }
 
-var latestTrackInfo = {
+const latestTrackInfo = {
   name: null,
   artist: null,
 };
 
 function updateRecentTracks(JSONdata) {
-  var oTracks = new Array().concat(JSONdata.recenttracks.track);
+  const oTracks = new Array().concat(JSONdata.recenttracks.track);
 
-  var div = document.getElementById('c_data');
-  var pic =
+  const div = document.getElementById('c_data');
+  let pic =
     oTracks[0].image[1]['#text'] != '' ? oTracks[0].image[2]['#text'] : null;
-  var latestTrackTitle = truncateName(oTracks[0].name, 22);
-  var latestTrackArtist = truncateName(oTracks[0].artist['#text'], 22);
+  const latestTrackTitle = truncateName(oTracks[0].name, 22);
+  const latestTrackArtist = truncateName(oTracks[0].artist['#text'], 22);
 
   if (
     latestTrackTitle !== latestTrackInfo.name &&
@@ -48,22 +42,15 @@ function updateRecentTracks(JSONdata) {
     latestTrackInfo.name = latestTrackTitle;
     latestTrackInfo.artist = latestTrackArtist;
 
-    var rm_currently_playing = document.getElementById('current_track');
+    const rm_currently_playing = document.getElementById('current_track');
     if (rm_currently_playing != null)
       rm_currently_playing.parentNode.removeChild(rm_currently_playing);
-    //include case such that no image is found
+    // include case such that no image is found
     pic = pic == null ? 'img/no_album_artwork.jpg' : pic;
     $('#c_data').slick(
       'slickAdd',
-      '<div> <br />' +
-        '<img class= "album_artwork" src="' +
-        pic +
-        '"</>' +
-        '<strong>"' +
-        latestTrackTitle +
-        '"</strong><br />' +
-        latestTrackArtist +
-        '<img id="current_track" src="img/musicbaryt3.gif" /></div>',
+      `${'<div> <br />' + '<img class= "album_artwork" src="'}${pic}"</>` +
+        `<strong>"${latestTrackTitle}"</strong><br />${latestTrackArtist}<img id="current_track" src="img/musicbaryt3.gif" /></div>`,
       0,
       true
     );
@@ -71,17 +58,17 @@ function updateRecentTracks(JSONdata) {
 }
 
 function lfmRecentTracks(JSONdata) {
-  var oTracks = new Array().concat(JSONdata.recenttracks.track);
+  const oTracks = new Array().concat(JSONdata.recenttracks.track);
 
   latestTrackInfo.name = truncateName(oTracks[0].name, 22);
   latestTrackInfo.artist = truncateName(oTracks[0].artist['#text'], 22);
 
-  for (var i = 0; i < oTracks.length; i++) {
-    //var div = document.getElementById('c_data');
-    var pic =
+  for (let i = 0; i < oTracks.length; i++) {
+    // var div = document.getElementById('c_data');
+    let pic =
       oTracks[i].image[1]['#text'] != '' ? oTracks[i].image[2]['#text'] : null;
-    var track_title = truncateName(oTracks[i].name, 22);
-    var artist = truncateName(oTracks[i].artist['#text'], 22);
+    const track_title = truncateName(oTracks[i].name, 22);
+    const artist = truncateName(oTracks[i].artist['#text'], 22);
 
     pic = pic == null ? 'img/no_album_artwork.jpg' : pic;
     currently_indicator = !oTracks[i].date
@@ -89,20 +76,12 @@ function lfmRecentTracks(JSONdata) {
       : '';
     $('#c_data').slick(
       'slickAdd',
-      '<div> <br />' +
-        '<img class= "album_artwork" src="' +
-        pic +
-        '"</>' +
-        '<strong>"' +
-        track_title +
-        '"</strong><br />' +
-        artist +
-        currently_indicator +
-        '</div>'
+      `${'<div> <br />' + '<img class= "album_artwork" src="'}${pic}"</>` +
+        `<strong>"${track_title}"</strong><br />${artist}${currently_indicator}</div>`
     );
   }
 
-  $('#messages').slideDown('slow', function() {
+  $('#messages').slideDown('slow', () => {
     $('#c_data').fadeIn(1000);
   });
 }
