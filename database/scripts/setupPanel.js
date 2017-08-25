a0146d4bd9f2722416a25d985b10e6d349f55785
Nathan Smith <nathan@nathansmith.io>
2017-08-25 06:49:42 -0700

    Add post-checkout hook to run yarn (#130)
    * Add postcheckout yarn command
    
    * Update documentation and script for yarn
    
    * Fix numbering on mac instructions
    
    * Add Cairo to install script. Fixes #131
    
    * Update grunt
    
    * Run mongod from script so user doesn’t have to do it
    
    * Correct database seeding
    
    * Kill mongo

--- database/scripts/setupPanel.js
+++ database/scripts/setupPanel.js
@@ -1,5 +1,5 @@
 // setupPanel.js
-// Set up user panel with first account: 
+// Set up user panel with first account:
 //
 // - username: gm
 // - password: [whatever is in passwords.json under 'gmpass']
@@ -9,36 +9,51 @@
 var accounts = require('../accounts');
 var passwords = require('../../passwords.json');
 
-console.log("Setting up Secret DJ Panel...");
+console.log('Setting up Secret DJ Panel...');
 
 // links to make available to managers
 var links = [];
-var managerPanel = {title: "Manager Panel", link: "/panel/manager"};
+var managerPanel = { title: 'Manager Panel', link: '/panel/manager' };
 links.push(managerPanel);
 
 /***** Create Manager Privilege *****/
 
-accounts.addPrivilege(accounts.managerPrivilegeName, links, function(err, success) {
-
+accounts.addPrivilege(accounts.managerPrivilegeName, links, function(
+  err,
+  success
+) {
   /***** Create General Manager Account *****/
 
   // requestNewAccount = function(username, pass, email, fullName, callback)
-  accounts.requestNewAccount("gm", passwords.gmpass, "radio.web@media.ucla.edu", "General Manager", function(err, saved) {
-    if (err) {
-      console.log("error creating gm account:", err);
-      process.exit();
-    } else if (saved) {
-      accounts.verifyAccount("gm", function(err, o) {
-        if (err) { console.log("error validating gm user", err); }
+  accounts.requestNewAccount(
+    'gm',
+    passwords.gmpass,
+    'radio.web@media.ucla.edu',
+    'General Manager',
+    function(err, saved) {
+      if (err) {
+        console.log('error creating gm account:', err);
+        process.exit();
+      } else if (saved) {
+        accounts.verifyAccount('gm', function(err, o) {
+          if (err) {
+            console.log('error validating gm user', err);
+          }
 
-        /***** Add new manager account to privilege *****/
+          /***** Add new manager account to privilege *****/
 
-        // updatePrivilege = function(username, privilege, shouldHave, callback)
-        accounts.updatePrivilege("gm", accounts.managerPrivilegeName, true, function(err, success) {
-          console.log("Finished setting up Secret DJ Panel");
-          process.exit();
+          // updatePrivilege = function(username, privilege, shouldHave, callback)
+          accounts.updatePrivilege(
+            'gm',
+            accounts.managerPrivilegeName,
+            true,
+            function(err, success) {
+              console.log('Finished setting up Secret DJ Panel');
+              process.exit();
+            }
+          );
         });
-      });
+      }
     }
-  });
+  );
 });
