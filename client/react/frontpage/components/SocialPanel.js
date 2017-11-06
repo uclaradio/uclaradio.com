870938bceb9cff34bdb89a4e757fad9a5f073e43
Alex X Xu <xualexander1@gmail.com>
2017-11-06 10:31:55 -0800

    Fixes made, but unsure if functions should be placed inside or outside  stateless component

--- client/react/frontpage/components/SocialPanel.js
+++ client/react/frontpage/components/SocialPanel.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 import './SocialPanel.scss';
 
-const SocialPanel = ({ links, size }) => (
-  <div className="link-container">
-    {links.map(function(obj) {
-      let faName = 'fa fa-' + obj.img + ' fa-' + size;
-      return (
+const SocialPanel = ({ links, size }) => {
+  const getFaName = (img, size) => `fa fa-${img} fa-${size}`;
+  return (
+    <div className="link-container">
+      {links.map(obj => (
         <a href={obj.link} className="social-link" target="_blank">
-          <i className={faName} aria-hidden="true" />
+          <i className={getFaName(obj.img, size)} aria-hidden="true" />
         </a>
-      );
-    })}
-  </div>
-);
+      ))}
+    </div>
+  );
+};
 
 export default SocialPanel;
