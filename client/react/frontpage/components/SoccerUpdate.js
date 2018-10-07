81a9f5f2c155ab27a38d903e73a3d2a74caef158
haejinjo <professionalhaejin@gmail.com>
2018-10-06 18:02:26 -0700

    DJ Anish needs a plug link to his live commentary on UCLA soccer games

---
+++ client/react/frontpage/components/SoccerUpdate.js
@@ -0,0 +1,22 @@
+//SoccerUpdate.js
+
+import React from 'react';
+import './SoccerUpdate.scss';
+
+const SoccerUpdate = () => (
+  <div id="container">
+    <div id="soccer">⚽</div>
+    <div id="linkContainer">
+      <a
+        class="blinkText"
+        href="https://uclabruins.com/sports/2016/8/16/live-audio.aspx"
+      >
+        Click for live coverage of UCLA Men’s Soccer vs Oregon State on Sunday
+        10/07 @ 12PM PST feat. UCLA Radio DJ Anish Vishwakoti
+      </a>
+    </div>
+    <div id="soccer">⚽</div>
+  </div>
+);
+
+export default SoccerUpdate;
