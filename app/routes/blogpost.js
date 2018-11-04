c8479f57e500aac3f128b1e8f8d068f89188eedd
Ishaan Pota <ishaanpota@Ishaans-MacBook-Pro.local>
2018-11-04 07:26:40 -0800

    The textbox sends data for the title and content

--- app/routes/blogpost.js
+++ app/routes/blogpost.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const post = require('../database/blogposts');
 
 router.post('/addPost', (req, res) => {
+  console.log('Hi i am here123');
   var title = req.body.title;
   var content = req.body.content;
   post.addPost(title, content, function(result) {
