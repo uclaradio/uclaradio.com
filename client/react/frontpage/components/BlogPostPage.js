30f26b5e49d0003fa269dc979427bb506266ac42
Tanzeela Khan <tnkhan8042@gmail.com>
2018-12-24 17:58:59 -0800

    renamed article to post (#261)

--- client/react/frontpage/components/BlogPostPage.js
+++ client/react/frontpage/components/BlogPostPage.js
@@ -8,44 +8,44 @@ Page content for individual post
 Displays full description of a post.. everything
 * */
 
-const keystoneAPI = '/getArticles';
+const keystoneAPI = '/getBlogPosts';
 const keystoneURL = 'http://localhost:3010';
 
 const BlogPostPage = React.createClass({
   getInitialState: function() {
-    return { blogPost: null };
+    return { fetching: true, post: null };
   },
   componentDidMount() {
     var queryRoute = `${keystoneAPI}/${this.props.params.blogPostID}`;
-    $.get(queryRoute, article => {
-      this.setState({ blogPost: article });
+    $.get(queryRoute, post => {
+      this.setState({ fetching: false, post: post });
     });
   },
   createMarkupForContent() {
-    if (this.state.blogPost.content) {
+    if (this.state.post.content) {
       return {
-        __html: this.state.blogPost.content,
+        __html: this.state.post.content,
       };
     } else {
       return;
     }
   },
   render() {
-    if (!this.state.blogPost) {
+    if (!this.state.post) {
       return (
         <div className="blogPostPage">
-          {this.props.fetching ? <Loader /> : "This post doesn't exist!"}
+          {this.state.fetching ? <Loader /> : "This post doesn't exist!"}
         </div>
       );
     }
-    const blogPost = this.state.blogPost;
-    const img = blogPost.image ? blogPost.image.filename : '';
+    const post = this.state.post;
+    const img = post.image ? post.image.filename : '';
     return (
       <div className="blogPostPage">
-        <div key={blogPost._id}>
-          <h1>{blogPost.name}</h1>
+        <div key={post._id}>
+          <h1>{post.name}</h1>
           <img
-            alt="blog post image"
+            alt="post image"
             style={{ width: '300px', height: '300px' }}
             src={keystoneURL + '/' + img}
           />
