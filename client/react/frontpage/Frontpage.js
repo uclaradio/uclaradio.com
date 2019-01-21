308f90f662fa5aaa371224dc5b322e5b1985daca
haejinjo <professionalhaejin@gmail.com>
2019-01-21 12:45:13 -0800

    W19 Show of the Week 2

--- client/react/frontpage/Frontpage.js
+++ client/react/frontpage/Frontpage.js
@@ -200,8 +200,8 @@ const Frontpage = React.createClass({
     this.interval = setInterval(this.props.updateNowPlaying, 30 * 1000);
 
     // update now playing and fetch initial shows data
-    // 01/14/19 - "Tea Time"
-    this.props.setSpotlightShowID(680);
+    // 01/21/19 - "Half-Time"
+    this.props.setSpotlightShowID(764);
     this.props.updateShows();
   },
   componentWillUnmount() {
