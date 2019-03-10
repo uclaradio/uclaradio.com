65eb6aa8871f834951ce9e877fe8d393ca0274cc
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2019-03-10 14:02:55 -0700

    Changed image parsing to regex to fix issue (#353)

--- client/react/frontpage/components/BlogPostPage.js
+++ client/react/frontpage/components/BlogPostPage.js
@@ -28,31 +28,31 @@ const BlogPostPage = React.createClass({
   parseKeystonePost(post) {
     if (post.img1) {
       post.content = post.content.replace(
-        '&lt;Image1&gt;',
+        /&lt;.*?Image1.*?&gt;/,
         `<img class="content-img" alt="Image 1" src=${post.img1.secure_url} />`
       );
     }
     if (post.img2) {
       post.content = post.content.replace(
-        '&lt;Image2&gt;',
+        /&lt;.*?Image2.*?&gt;/,
         `<img class="content-img" alt="Image 2" src=${post.img2.secure_url} />`
       );
     }
     if (post.img3) {
       post.content = post.content.replace(
-        '&lt;Image3&gt;',
+        /&lt;.*?Image3.*?&gt;/,
         `<img class="content-img" alt="Image 3" src=${post.img3.secure_url} />`
       );
     }
     if (post.img4) {
       post.content = post.content.replace(
-        '&lt;Image4&gt;',
+        /&lt;.*?Image4.*?&gt;/,
         `<img class="content-img" alt="Image 4" src=${post.img4.secure_url} />`
       );
     }
     if (post.img5) {
       post.content = post.content.replace(
-        '&lt;Image5&gt;',
+        /&lt;.*?Image5.*?&gt;/,
         `<img class="content-img" alt="Image 5" src=${post.img5.secure_url} />`
       );
     }
