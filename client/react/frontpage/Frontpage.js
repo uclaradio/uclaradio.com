c6915e78e580294c1a54707c61c11c0df0ece248
haejinjo <professionalhaejin@gmail.com>
2019-05-04 07:13:05 -0700

    S19 Show of the Week 5 + Add Graphix Page

--- client/react/frontpage/Frontpage.js
+++ client/react/frontpage/Frontpage.js
@@ -223,8 +223,8 @@ const Frontpage = React.createClass({
     this.interval = setInterval(this.props.updateNowPlaying, 30 * 1000);
 
     // update now playing and fetch initial shows data
-    // 04/22/19 - "fever dream hospital"
-    this.props.setSpotlightShowID(805);
+    // 04/29/19 - "Identity Crisis: Being Asian in America"
+    this.props.setSpotlightShowID(827);
     this.props.updateShows();
   },
   componentWillUnmount() {
